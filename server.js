aa84ac562edc0ede76379fa6fd0e7e8b7aaac16d
Shabbir Salem <S_Salem52@hotmail.com>
2023-12-03 21:06:21 -0500

    first

--- server.js
+++ server.js
@@ -2,18 +2,40 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
+//GET Route
+app.get('/projectData', sendData);
+
+function sendData (request, response) {
+  response.send(projectData);
+};
+
+app.post('/addWeatherData', addData);
+
+function addData(req, res){
+  projectData["temp"] = req.body.temp;
+  projectData["date"] = req.body.date;
+  projectData["feelings"] = req.body.feelings;
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,72 @@
 /* Global Variables */
+const baseURI = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = '&appid=b08f99ec50b160ba0b254c262733988a&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+    const newZip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    getOpenWeatherData(baseURI,newZip, apiKey)
+    .then(function(data){
+
+        postData('/addWeatherData', {temp:data.main.temp, date:newDate, feelings:feelings})
+    })
+    .then(
+        updateUI()
+    )
+}
+
+//Async GET Request function to OpenWeather API
+    const getOpenWeatherData = async (baseURI, zip, key)=>{
+
+        const res = await fetch(baseURI+zip+key)
+        try {
+      
+          const data = await res.json();
+        //   console.log(data)
+          return data;
+        }  catch(error) {
+          console.log("error", error);
+          // appropriately handle the error
+    }
+}
+
+
+// Post Data
+const postData = async (url = '', data = {})=>{
+console.log(data)
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        // console.log(newData);
+        return newData
+    }catch(error) {
+        console.log("error", error);
+    }
+
+}
+
+const updateUI = async () => {
+    const request = await fetch('/projectData');
+    try{
+        const allData = await request.json();
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('content').innerHTML = allData.feelings;
+    }catch(error){
+        console.log("error", error);
+      }
+    }
